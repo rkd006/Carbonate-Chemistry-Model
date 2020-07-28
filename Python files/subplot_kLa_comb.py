b5944d07163b3700fcd3ffbf21b7572057edaa05
rkd006 <rkd006@bucknell.edu>
2020-07-27 18:10:23 -0700

    7/27

--- Python files/subplot_kLa_comb.py
+++ Python files/subplot_kLa_comb.py
@@ -24,14 +24,18 @@ pHend = 8.2
 delpH = 0.1
 d = 0.15
 plt.subplots(nrows = 1, ncols = 2, figsize=(9, 3),sharex= True, sharey= True)
-
-kLa = 0.5
+plt.subplot(1,2,2)
+kLa = 3
 y1 = calc_CO2_loss(pK1, pK2, kLa, d, CO2sat, pHin, pHend, delpH, alkin, alkend, delalk)
-plt.xlabel('pH')
+plt.gca().axes.get_yaxis().set_visible(False)
+plt.xlabel('b)')
 plt.legend(['alk = 2 meq/L', 'alk = 7 meq/L', 'alk = 12 meq/L', 'alk = 17 meq/L', 'alk = 22 meq/L', 'alk = 27 meq/L', 'alk = 32 meq/L'])
-plt.show()
-kLa = 3.0
+plt.axis([6.5, 8.2, 0, 4000])
+
+plt.subplot(1,2,1)
+kLa = 0.5
 y2 = calc_CO2_loss(pK1, pK2, kLa, d, CO2sat, pHin, pHend, delpH, alkin, alkend, delalk)
-plt.xlabel('pH')
+plt.xlabel('a)')
 plt.ylabel('$CO_2$ loss to the atmosphere (g $m^{-2}$ $day^{-1})$')
-plt.show()
+plt.axis([6.5, 8.2, 0, 4000])
+#plt.text(8.5, -800, str('pH'), fontsize=10, ha='center')
