a8c3dc13f5f4da2a3b3b8fe1bb8b2e17e4048a33
rkd006 <rkd006@bucknell.edu>
2020-08-09 16:59:39 -0400

    8/9

--- Python files/monod_simple.py
+++ Python files/monod_simple.py
@@ -24,6 +24,19 @@ plt.xlabel('light intensity (W/m$^2$)')
 plt.ylabel('specific growth rate (day$^{-1})$')
 plt.show()
 
+#Beer Lambert Law
+from scipy import integrate
+I0 = 13.06 #W/m2
+o = 0.175 #m2/g
+X = 50 #g/m3
+z = np.arange(0, 0.18, 0.01)
+I = I0*np.exp(-o*X*z)
+I_int = integrate.cumtrapz(I, z, initial=0)
+plt.plot(z, I_int)
+plt.xlabel('position (cm)')
+plt.ylabel('Light Intensity (W m$^{-2})$')
+plt.show()
+
 umax = 1.44
 I = 186
 Ki = 178.7/4.6
