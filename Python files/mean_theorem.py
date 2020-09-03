119b76f5c2fd60e4d59fde442a01b2451e610075
rkd006 <rkd006@bucknell.edu>
2020-09-03 19:34:48 -0400

    9/3

--- Python files/mean_theorem.py
+++ Python files/mean_theorem.py
@@ -1,15 +1,34 @@
 #author: Riley Doyle
 #date: 9/3/20
 #file: mean_theorem
-#status: WORKING
+#status: NOT WORKING
 
-d = 0.15 #m
+import numpy as np
+import scipy.integrate as integrate
+
+#define variables
 kd = 0 #1/day #not added yet
 K = 160 #g/m2
 umax = 3.2424 #1/day
 Ki = 13.9136 #W/m2
 I = 100 #W/m2
-X0 = 0.04 #g/m2
 C = ((umax*I)/(I + Ki))
-dX/dt = C*(1-(X/K))*X
-X = K/(np.exp(-(C*t + C1))-1)
+
+#solve for C1
+t = 0
+X = 0.04 #g/m2
+C1 = -(np.log10((K/X)+1) + C*t)
+
+
+#function for biomass density
+X = lambda x:(K/(np.exp(-(C*t + C1)) - 1))
+
+#limits
+a = 0
+b = 4
+
+integral = integrate.quad(X, a, b)
+print (integral)
+
+meanbiomass = (1/(b - a))*integral[0]
+print (meanbiomass)
